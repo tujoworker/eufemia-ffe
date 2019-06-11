59b0eaf7b3fa37d961bcbd273a0ae175f59996c0
Tobias Høegh <tobias@tujo.no>
2019-06-11 15:06:17 +0200

    add appearance helper on carrier switch

--- src/layout/App.js
+++ src/layout/App.js
@@ -1,5 +1,6 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
+import classnames from 'classnames'
 
 import { Context } from '../hacked-ffe/Provider'
 
@@ -8,8 +9,14 @@ import './styles/App.less'
 
 import Switch from '../hacked-ffe/Switch'
 
+let timeoutId
 const App = ({ children }) => {
   const { carrier, setCarrier } = useContext(Context)
+  const [isDimming, setDimming] = useState(false)
+
+  // on unmount
+  useEffect(() => () => clearTimeout(timeoutId), [])
+
   return (
     <div className="app-wrapper">
       <div className="app-wrapper-inner">
@@ -17,8 +24,9 @@ const App = ({ children }) => {
           <Switch
             defaultChecked={carrier === 'dnb'}
             onChange={({ checked }) => {
-              setTimeout(() => {
-                console.log('onChange', checked)
+              setDimming(true)
+              timeoutId = setTimeout(() => {
+                // setDimming(false)// is not needed
                 setCarrier(checked ? 'dnb' : 'ffe')
               }, 500)
             }}
@@ -26,7 +34,9 @@ const App = ({ children }) => {
             Eufemia is Enabled
           </Switch>
         </header>
-        <main className="app-content">{children}</main>
+        <main className={classnames('app-content', isDimming && 'dimmed')}>
+          {children}
+        </main>
         <footer className="app-footer">Happy hacking {'🍒'}</footer>
       </div>
     </div>
